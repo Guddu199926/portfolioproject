ea521b8daee61c7c2de8cee20115adbabbe3ed3f
Guddu199926 <mannepalliravi999@gmail.com>
2024-06-24 14:27:27 -0400

    added image and description

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ with col1:
 with col2:
     st.title("Ravi Charan")
     content =  """
-          Hi, I am Ravi! All I do is eat sleep and enjoy. Please help me to continue this process
+          Hi, I am Ravi! All I do is eat sleep and enjoy. Please help me to continue this process. Alcohol is not good for human since it effect the liver and causes obesity.
             """
     st.info(content)
+
+    content2 ="""Below you can find some of the apps I have built in Python. Feel free to contact me!
+               """
+    st.info(content2)
