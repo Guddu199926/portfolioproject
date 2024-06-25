fb01d4a872e9fe6d68184c2aff5fef66c1baa311
Guddu199926 <mannepalliravi999@gmail.com>
2024-06-25 15:32:45 -0400

    added two columns and changed image

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    image=st.image("images/mypic.png")
 with col2:
     st.title("Ravi Charan")
     content =  """
@@ -12,6 +13,16 @@ with col2:
             """
     st.info(content)
 
-    content2 ="""Below you can find some of the apps I have built in Python. Feel free to contact me!
+content2 ="""Below you can find some of the apps I have built in Python. Feel free to contact me!
                """
-    st.info(content2)
+st.info(content2)
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep = ";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
