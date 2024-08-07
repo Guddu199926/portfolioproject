3e3d9fe574413d009a61ed415de647a980272875
Guddu199926 <mannepalliravi999@gmail.com>
2024-08-07 12:18:08 -0400

    added url, and shorten the code.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+def read_rows(rows):
+    for index, row in rows.iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code](row['url'])")
+
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
@@ -16,13 +23,12 @@ with col2:
 content2 ="""Below you can find some of the apps I have built in Python. Feel free to contact me!
                """
 st.info(content2)
-col3, col4 = st.columns(2)
+col3, empty_col ,col4 = st.columns([1.5,0.5,1.5])
 
 df = pd.read_csv("data.csv", sep = ";")
-with col3:
-    for index, row in df[:10].iterrows():
-        st.header(row["title"])
-with col4:
-    for index, row in df[10:].iterrows():
-        st.header(row["title"])
 
+with col3:
+    read_rows(df[:10])
+
+with col4:
+    read_rows(df[10:])
